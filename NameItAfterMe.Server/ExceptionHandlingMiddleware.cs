644f713000d1f08ca03cb0f29a550065a6584089
Joshua Campbell <JULC14@outlook.com>
2022-08-05 20:06:23 -0700

    Add Middlware and revisions

---
+++ NameItAfterMe.Server/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+
+namespace NameItAfterMe.Server;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next) => _next = next;
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (ValidationException e)
+        {
+            context.Response.StatusCode = 422;
+            await context.Response.WriteAsJsonAsync(e.Errors);
+        }
+        catch (Exception)
+        {
+            context.Response.StatusCode = 500;
+        }
+    }
+}
